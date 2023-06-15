67481ed9d09e6fef1ac5ec959f250c88d555d97a
TDR <darkrabitthe@gmail.com>
2023-06-15 19:56:11 +0300

    guestService impl

--- src/main/java/com/example/hoteladministrator/services/GuestService.java
+++ src/main/java/com/example/hoteladministrator/services/GuestService.java
@@ -1,4 +1,33 @@
 package com.example.hoteladministrator.services;
 
+import com.example.hoteladministrator.entities.Guest;
+import com.example.hoteladministrator.repositories.GuestRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class GuestService {
+    private final GuestRepository guestRepository;
+    public Guest getGuestById(long id){
+        Optional<Guest> guest = guestRepository.findById(id);
+        if (guest.isEmpty())
+            throw new RuntimeException("No guest was found");
+        return guest.get();
+    }
+    public List<Guest> getGuestList(){
+        return guestRepository.findAll();
+    }
+     public void addGuest(Guest guest){
+        guestRepository.save(guest);
+     }
+     public void deleteGuest(Guest guest){
+        guestRepository.delete(guest);
+     }
+     public void deleteGuestById(long id){
+        guestRepository.deleteById(id);
+     }
 }
